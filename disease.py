99b761aeb3c7acfd1e4cd558742c88a568115c40
jeevandas-jd <jeevandasms2004@gmail.com>
2024-09-20 18:57:45 +0530

    data-collection

--- disease.py
+++ disease.py
@@ -2,6 +2,8 @@ import bfsLink as bl
 import requests
 from bs4 import BeautifulSoup
 
+import re
+
 with open("root.text","r") as f:
 
     lines=f.readlines()
@@ -20,14 +22,51 @@ def ExtactDisease(url,class_name):
         for n in names:
             name=n.get("href")
             f.write(f"{n.text} \t link :{name} \n")
+    with open("diseaselink.txt","a") as f:
+        print("call 2")
+        for n in names:
+            name=n.get("href")
+            f.write(f"{name} \n")
 
 base_class="cmp-anchor--plain cmp-button cmp-button__link cmp-result-name__link"
 
-ExtactDisease(links[4],base_class)
-
+"""ExtactDisease(links[4],base_class)"""
+"""
 for i in range(1,len(links)):
 
-    ExtactDisease(links[i],base_class)
+    ExtactDisease(links[i],base_class)"""
+def ExtractSymptom(url):
+    response=requests.get(url)
+    print(response)
+
+    soup=BeautifulSoup(response.text,"html.parser")
+    
+
+    symptom=soup.find("div",class_="content")
+    
+    pattern = r'diseases-conditions/([^/]+)' 
+    match=re.search(pattern,url)
+    name=match.group(1)
+
+    symptom=symptom.get_text().strip()
+
+    pattern2 = r'Symptoms\s*(.*?)\s*(?=Causes|When to see a doctor)'
+    match = re.search(pattern2, symptom, re.DOTALL)
+    symptom = match.group(1).strip()
+
+    with open(f"{name}.txt","w") as f:
+        f.write(symptom.strip())
+
+
+with open("diseaselink.txt","r") as r:
+    lines=r.readlines()
+
+
+links=[line.strip() for line in lines]
+
+
+
+
 
 
 
